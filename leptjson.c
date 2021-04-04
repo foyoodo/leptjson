50aa77a26f01b2f630014741d56adeb9c6a7268a
foyoodo <foyoodo@gmail.com>
2021-04-04 22:42:34 +0800

    Part02

--- leptjson.c
+++ leptjson.c
@@ -1,7 +1,7 @@
 #include "leptjson.h"
 
 #include <assert.h> /* assert() */
-#include <stdlib.h> /* NULL */
+#include <stdlib.h> /* NULL, strtod() */
 
 #define EXPECT(c, ch)             \
     do {                          \
@@ -47,6 +47,16 @@ static int lept_parse_false(lept_context* c, lept_value* v) {
     return LEPT_PARSE_OK;
 }
 
+static int lept_parse_number(lept_context* c, lept_value* v) {
+    char* end;
+    /* \TODO validate number */
+    v->n = strtod(c->json, &end);
+    if (c->json == end) return LEPT_PARSE_INVALID_VALUE;
+    c->json = end;
+    v->type = LEPT_NUMBER;
+    return LEPT_PARSE_OK;
+}
+
 static int lept_parse_value(lept_context* c, lept_value* v) {
     switch (*c->json) {
         case 'n':
@@ -55,10 +65,10 @@ static int lept_parse_value(lept_context* c, lept_value* v) {
             return lept_parse_true(c, v);
         case 'f':
             return lept_parse_false(c, v);
+        default:
+            return lept_parse_number(c, v);
         case '\0':
             return LEPT_PARSE_EXPECT_VALUE;
-        default:
-            return LEPT_PARSE_INVALID_VALUE;
     }
 }
 
@@ -72,6 +82,7 @@ int lept_parse(lept_value* v, const char* json) {
     if ((ret = lept_parse_value(&c, v)) == LEPT_PARSE_OK) {
         lept_parse_whitespace(&c);
         if (*c.json != '\0') {
+            v->type = LEPT_NULL;
             ret = LEPT_PARSE_ROOT_NOT_SINGULAR;
         }
     }
@@ -81,4 +92,9 @@ int lept_parse(lept_value* v, const char* json) {
 lept_type lept_get_type(const lept_value* v) {
     assert(v != NULL);
     return v->type;
+}
+
+double lept_get_number(const lept_value* v) {
+    assert(v != NULL && v->type == LEPT_NUMBER);
+    return v->n;
 }
